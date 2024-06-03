533c428df3c1b9bcb3045a604c70aa3ca73d35d2
Daria Senko <dariaxdee1@o2.pl>
2024-06-03 19:32:41 +0200

    Tworzenie i usuwanie elementów DOM

---
+++ index.js
@@ -0,0 +1,11 @@
+const div = document.createElement('div');
+div.id = 'product';
+div.classItem.add('element');
+
+const container = document.querySelector('#about');
+container.appendChild(div);
+
+const div = document.querySelector('#about .produkt');
+
+const container = document.querySelector('#about');
+container.removeChild(div);
